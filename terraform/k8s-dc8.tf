f397e53e1313688e0dc924d675176782f2b68305
Jason Whitelock <jase@oshelp.co.uk>
2021-03-30 11:01:42 +0100

    add terraform

---
+++ terraform/k8s-dc8.tf
@@ -0,0 +1,17 @@
+module "k8s-dc8" {
+  source    = "./modules/k8s-node/"
+  
+  qemu_host      = "qemu+ssh://jason@192.168.167.8/system"
+  qemu_datastore = "Data"
+
+  vm_name   = "k8s-dc8"
+  mem_gb    = 8
+  num_cpu   = 4
+  autostart = true
+
+  net_interface = "br0"
+  ip_address   = "192.168.167.16"
+
+  os_size_gb   = 20
+  data_size_gb = 60
+}
---
+++ terraform/k8s-rig.tf
@@ -0,0 +1,17 @@
+module "k8s-rig" {
+  source    = "./modules/k8s-node/"
+  
+  qemu_host      = "qemu:///system"
+  qemu_datastore = "FD-SSD"
+
+  vm_name   = "k8s-rig"
+  mem_gb    = 8
+  num_cpu   = 4
+  autostart = true
+
+  net_interface = "br0"
+  ip_address   = "192.168.167.17"
+
+  os_size_gb   = 20
+  data_size_gb = 60
+}
