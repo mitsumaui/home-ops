4ab4be3f73f6fee8143ee869f5a435784ba13ad5
Jason Whitelock <jase@oshelp.co.uk>
2021-04-14 23:19:17 +0100

    host changes

--- terraform/k8s-hpd.tf
+++ terraform/k8s-hpd.tf
@@ -1,16 +1,16 @@
-module "k8s-dc8" {
+module "k8s-hpd" {
   source    = "./modules/k8s-node/"
   
   qemu_host      = "qemu+ssh://jason@192.168.167.8/system"
   qemu_datastore = "Data"
 
-  vm_name   = "k8s-dc8"
+  vm_name   = "k8s-hpd"
   mem_gb    = 12
   num_cpu   = 4
   autostart = true
 
   net_interface = "br0"
-  ip_address   = "192.168.167.16"
+  ip_address   = "192.168.167.14"
 
   os_size_gb   = 20
   data_size_gb = 60
